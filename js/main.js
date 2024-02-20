4ff935b38e45eae78e637895c5a1cee5c6f6b091
Connie Chin <conniec96@gmail.com>
2024-02-20 05:32:35 +0000

    Finished all steps, working on CSS.

--- js/main.js
+++ js/main.js
@@ -1,39 +1,28 @@
-/* global data */
-interface FormElements extends HTMLFormControlsCollection {
-  title: HTMLInputElement;
-  photoUrl: HTMLInputElement;
-  notes: HTMLTextAreaElement;
-  entryId?: number;
-}
-
+'use strict';
 const $photoUrl = document.querySelector('#photoUrl');
 if (!$photoUrl) throw new Error('The $photoUrl query failed');
 const $image = document.querySelector('img');
 if (!$image) throw new Error('The $image query failed');
 const $form = document.querySelector('form');
 if (!$form) throw new Error('The $form query failed');
-
-function forPhotoUrl(event: Event): void {
+function forPhotoUrl(event) {
   const $eventTarget = event.target.value;
   $image.src = $eventTarget;
 }
-
 $photoUrl.addEventListener('input', forPhotoUrl);
-
-function forFormSubmit(event: Event): void {
+function forFormSubmit(event) {
   event.preventDefault();
-  const $formElements = $form.elements as FormElements;
+  const $formElements = $form.elements;
   const formObject = {
     title: $formElements.title.value,
     photoUrl: $formElements.photoUrl.value,
     notes: $formElements.notes.value,
   };
   // console.log('$formObject:', formObject);
-  formObject.entryId = data.nextEntryId;
+  formObject['entryId'] = data.nextEntryId;
   data.nextEntryId++;
   data.entries.push(formObject);
   $image?.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
-
 $form.addEventListener('submit', forFormSubmit);
